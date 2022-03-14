6627682050d690565f90c228d85cf5caced62711
vaibhavhooda <vaibhav.hooda1@gmail.com>
2022-03-13 23:55:06 -0400

    Adding resources to the project

--- src/main/java/com/dav/LMSConfig.java
+++ src/main/java/com/dav/LMSConfig.java
@@ -1,10 +1,25 @@
 package com.dav;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.context.annotation.ComponentScan;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan({ "com.dav", "controller" })
-public class LMSConfig {
-
+public class LMSConfig extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,17 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Welcome</title>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+	<link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
+    <script src="<c:url value="/resources/js/main.js" />"></script>
 </head>
 <body>
+	<h1>1. Test CSS</h1>
+
+	<h2>2. Test JS</h2>
+	<div id="msg"></div>
 	<h1>Welcome to the Library Management System</h1>
 	<br>
 	<form action="member" method="get">
